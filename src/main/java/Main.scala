866a4a9e292b48a865847b956ff0e6c319658082
starqiu <674867812@qq.com>
2016-04-29 19:08:48 +0800

    init spark & maven &scala env

--- src/main/java/Main.scala
+++ src/main/java/Main.scala
@@ -1,11 +1,13 @@
+import org.apache.spark.SparkContext
+
 /**
   * Created by starqiu on 16-4-29.
   */
 object Main {
 
   def main(args: Array[String]): Unit ={
-    print("Hello World")
+    val sc = new SparkContext("spark://localhost:7077","Hello World")
+    val file = sc.textFile("hdfs:///usr/hello")
   }
 }
-
 
